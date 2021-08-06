c6bc483257cd0bcfcebee6c264dc10c0e1c0b616
Ariffudin <ariffudinnotresponding@gmail.com>
2021-08-06 19:29:27 +0700

    Hello World

---
+++ 1-hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>New in React</h2>
+    </div>    
+  );
+}
+
+export default App;
